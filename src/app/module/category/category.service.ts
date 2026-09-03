8215ea98945872dbf704d393e775de8d45c21c8d
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-09-03 19:23:15 +0600

    complete category management

---
+++ src/app/module/category/category.service.ts
@@ -0,0 +1,154 @@
+import { prisma } from "../../lib/prisma";
+import { AppError } from "../../utils/AppError";
+import {
+  ICreateCategoryPayload,
+  IUpdateCategoryPayload,
+} from "./category.interface";
+import httpStatus from "http-status";
+
+const createCategory = async (payload: ICreateCategoryPayload) => {
+  const name = payload.name.trim();
+
+  if (!name) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Category name is required!");
+  }
+
+  const existingCategory = await prisma.category.findFirst({
+    where: {
+      name: {
+        equals: name,
+        mode: "insensitive"
+      }
+    },
+  });
+
+  if (existingCategory) {
+    throw new AppError(httpStatus.CONFLICT, "Category already exists!");
+  }
+
+  const category = await prisma.category.create({
+    data: {
+      name,
+      description: payload.description?.trim() || null,
+    },
+  });
+
+  return category;
+};
+
+const getAllCategories = async () => {
+  const categories = await prisma.category.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return categories;
+};
+
+const getSingleCategory = async (id: string) => {
+  const category = await prisma.category.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!category) {
+    throw new AppError(httpStatus.NOT_FOUND, "Category not found!");
+  }
+
+  return category;
+};
+
+const updateCategory = async (id: string, payload: IUpdateCategoryPayload) => {
+  const existingCategory = await prisma.category.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!existingCategory) {
+    throw new AppError(httpStatus.NOT_FOUND, "Category not found!");
+  }
+
+  if (payload.name) {
+    const name = payload.name.trim();
+
+    const duplicateCategory = await prisma.category.findFirst({
+      where: {
+        name,
+        NOT: {
+          id,
+        },
+      },
+    });
+
+    if (duplicateCategory) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        "Another category with this name already exists!",
+      );
+    }
+
+    payload.name = name;
+  }
+
+  if (payload.description) {
+    payload.description = payload.description.trim();
+  }
+
+  const updatedCategory = await prisma.category.update({
+    where: {
+      id,
+    },
+    data: {
+      ...(payload.name && { name: payload.name }),
+      ...(payload.description !== undefined && {
+        description: payload.description,
+      }),
+    },
+  });
+
+  return updatedCategory;
+};
+
+const deleteCategory = async (id: string) => {
+  const existingCategory = await prisma.category.findUnique({
+    where: {
+      id,
+    },
+    include: {
+        _count: {
+            select: {
+                products: true
+            }
+        }
+    }
+  });
+  if (!existingCategory) {
+    throw new AppError(httpStatus.NOT_FOUND, "Category not found!");
+  }
+
+  if (existingCategory._count.products > 0) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Cannot delete category because it contains associated products!",
+    );
+  }
+
+  const category = await prisma.category.delete({
+    where: {
+      id,
+    },
+  });
+
+  return category;
+};
+
+export const CategoryService = {
+  createCategory,
+  getAllCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+};
